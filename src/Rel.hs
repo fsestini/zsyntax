e9e0e7460740c087b5bdcfd1fd4d9eb3c2a76b5a
Filippo Sestini <sestini.filippo@gmail.com>
2017-04-16 02:13:00 +0200

    Rel.hs: Add haddock to Rel type

--- src/Rel.hs
+++ src/Rel.hs
@@ -16,6 +16,17 @@ import Filterable
 import Control.Monad
 import Control.Monad.Fail
 
+{-| A Rel object represents a curried function with an unbounded, unspecified
+    number of input arguments, possibly zero.
+    Rel elements live in the Maybe monad, so they represent three possible
+    situations:
+
+    1. A failing computation which produces nothing;
+    2. A successful computation that produces a trivial curried function
+       accepting zero arguments (that is, a single value of the return type);
+    3. A successful computation that produces a curried function, that is
+       a function accepting one argument and returning a new value of type Rel.
+ -}
 data Rel a b = Rel { unRel :: Maybe (Either b (a -> Rel a b)) }
 
 filterPartitionRel :: (Filterable t, Functor t) => t (Rel a b) -> (t b, t (a -> Rel a b))
