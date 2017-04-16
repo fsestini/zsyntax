c6e8f05a0c0ccef63325325e4b3603fb2f92dc4f
Filippo Sestini <sestini.filippo@gmail.com>
2017-04-16 02:15:19 +0200

    Prover.Transformer: Use type of active sequents from Prover.Class

--- src/Prover/Transformer.hs
+++ src/Prover/Transformer.hs
@@ -4,16 +4,18 @@ module Prover.Transformer where
 
 import qualified Data.Map as M
 import qualified Data.Set as S
+import LabelledSequent
 import Formula
 import Rule
 import Control.Monad.Trans.State.Strict
 import Control.Monad.Trans.Reader
+import Prover.Class
+
+type ActiveSequents l a = S.Set (ActiveSequent l a)
+type InactiveSequents l a = S.Set (LabelledSequent l a)
+newtype Formula a = F (forall p . LFormula p () a)
 
 type ProverState l a = ([Rule l a], ActiveSequents l a, InactiveSequents l a)
 type ProverEnvironment l a = (LabelledSequent l a, M.Map (Label l a) (Formula a))
-
-newtype ActiveSequents l a = AS (S.Set (LabelledSequent l a))
-newtype InactiveSequents l a = IS (S.Set (LabelledSequent l a))
-newtype Formula a = F (forall p . LFormula p () a)
 
 type ProverT l a m b = ReaderT (ProverEnvironment l a) (StateT (ProverState l a) m) b
