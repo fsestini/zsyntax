28a27555f39bac357e0b5a21de76c741839a6503
Filippo Sestini <sestini.filippo@gmail.com>
2017-04-17 00:21:04 +0200

    Prover.hs: WIP do stuff

--- src/Prover.hs
+++ src/Prover.hs
@@ -18,6 +18,8 @@ import Rel
 import Prover.Class
 import Prover.Transformer
 
+-- TODO: use the real thing
+type DerivationTerm l a = Int
 
 processNewActive
   :: (Monad m, HasProverState l a m)
@@ -29,16 +31,28 @@ processNewActive sequent = do
       r2 = percolate actives . resRules $ r1
   return $ r1 `mappend` r2
 
-type Derivation l a = LabelledSequent l a
+{-
 
-filterUnsubsumed
-  :: (HasProverState l a m, Monad m)
-  => S.Set (LabelledSequent l a) -> m (S.Set (LabelledSequent l a))
-filterUnsubsumed = undefined
+  1. Compute frontier
+  2. Compute initial set of rules ---> initial sequents
+  3. Put initial sequents in inactive set
+  4. Pop inactive sequent. If no more, goto
+  5. Put popped inactive in active set
+  6. Apply all rules to sequent
+  7. Percolate
+  8. Add resulting sequents that are not subsumed by old inactive sequents to
+     inactive set
+  9. Remove inactive sequents that are subsumed by the new sequents
+  10. Add rules to rule set
+  11. Is any new sequent subsumed the goal, exit
+  12. Add new sequents to inactive set
+  13. Goto 4
+
+-}
 
 otterLoop
   :: (Monad m, HasProverState l a m, HasProverEnvironment l a m)
-  => m (Maybe (Derivation l a))
+  => m (Maybe (DerivationTerm l a))
 otterLoop = do
   -- TODO!!! check here if any inactive sequent subsumes the goal sequent
   inactive <- popInactive
