4fe71d71d7e69564413b070563b1b5a2c720f6e3
Filippo Sestini <sestini.filippo@gmail.com>
2017-04-18 01:12:21 +0200

    Prover.Frontier: Rewrite module to account for rule generation.
    Use a more expressive type of decorated formulas, in order to distinguish
    between formulas in the frontier from which compute initial sequents and rules,
    and those from which we don't.

--- src/Prover/Frontier.hs
+++ src/Prover/Frontier.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Prover.Frontier
-  ( frontier
-  , DecLFormula(..)
+  ( initialSequentsAndRules
   ) where
 
 {-
@@ -16,42 +19,78 @@ module Prover.Frontier
 
 -}
 
+import Filterable
+import LabelledSequent
+import Rule
 import Formula
 import qualified Data.Set as S
+import Relation
+import Rel
+import Control.Arrow
 
 --------------------------------------------------------------------------------
 -- Decorated formulas
 
--- TODO: Don't I have to include polarity in the type?
-data DecLFormula :: * -> * -> * where
-  UnrestrNegativeDLF :: LFormula p l a -> DecLFormula l a
-  LinearNegativeDLF
-    :: (IsLeftSynchronous p)
-    => LFormula p l a -> DecLFormula l a
-  LinearPositiveDLF
-    :: (IsRightSynchronous p)
-    => LFormula p l a -> DecLFormula l a
+-- | Decoration kind.
+data FShape = NonAtomic | LBNAtom | RBNAtom | LBPAtom | RBPAtom
 
-instance Eq (DecLFormula l a) where
+type family NegFShape (b :: Bias) = (r :: FShape) | r -> b where
+  NegFShape LeftBias = LBNAtom
+  NegFShape RightBias = RBNAtom
+
+type family PosFShape (b :: Bias) = (r :: FShape) | r -> b where
+  PosFShape LeftBias = LBPAtom
+  PosFShape RightBias = RBPAtom
+
+class NonAtom (p :: Pole) where
+instance NonAtom LSRA where
+instance NonAtom LARS where
+
+-- TODO: Don't I have to include polarity in the type?
+data DecFormula :: FShape -> * -> * -> * where
+  UnrestrNegativeAtom :: Atom b a -> DecFormula (NegFShape b) l a
+  UnrestrNegative
+    :: (NonAtom p)
+    => LFormula p l a -> DecFormula NonAtomic l a
+  LinearNegativeAtom :: Atom b a -> DecFormula (NegFShape b) l a
+  LinearNegative :: LFormula LSRA l a -> DecFormula NonAtomic l a
+  LinearPositiveAtom :: Atom b a -> DecFormula (PosFShape b) l a
+  LinearPositive
+    :: LFormula LARS l a -> DecFormula NonAtomic l a
+
+data ODecFormula l a = forall s . ODF (DecFormula s l a)
+
+instance Eq (ODecFormula l a) where
   (==) = undefined
 
-instance Ord (DecLFormula l a) where
+instance Ord (ODecFormula l a) where
   compare = undefined
 
-toUnrestrNeg :: OLFormula l a -> DecLFormula l a
-toUnrestrNeg (OLF f) = UnrestrNegativeDLF f
+toUnrestrNeg :: OLFormula l a -> ODecFormula l a
+toUnrestrNeg (OLF (FAtom a)) = ODF . UnrestrNegativeAtom $ a
+toUnrestrNeg (OLF f@(FConj _ _ _)) = ODF . UnrestrNegative $ f
+toUnrestrNeg (OLF f@(FImpl _ _ _)) = ODF . UnrestrNegative $ f
 
-toLinearNeg :: OLSLFormula l a -> DecLFormula l a
-toLinearNeg (OLSLF f) = LinearNegativeDLF f
+toLinearNeg :: OLSLFormula l a -> ODecFormula l a
+toLinearNeg (OLSLF f) =
+  case decideLS f of
+    Left (OA atom) -> ODF . LinearNegativeAtom $ atom
+    Right formula -> ODF . LinearNegative $ formula
+
+toLinearPositive :: (IsRightSynchronous p) => LFormula p l a -> ODecFormula l a
+toLinearPositive f =
+  case decideRS f of
+    Left (OA atom) -> ODF . LinearPositiveAtom $ atom
+    Right formula -> ODF . LinearPositive $ formula
 
 --------------------------------------------------------------------------------
 -- Frontier computation
 
 -- | Computes the frontier of a labelled sequent.
-frontier :: Sequent l a -> S.Set (DecLFormula l a)
-frontier (S uc lc goal) =
+frontier :: Sequent l a -> S.Set (ODecFormula l a)
+frontier (SQ uc lc goal) =
   S.map toUnrestrNeg uc `S.union` (S.fromList . map toLinearNeg) lc `S.union`
-  S.singleton (LinearPositiveDLF goal) `S.union`
+  S.singleton (toLinearPositive goal) `S.union`
   unrestrFrontier `S.union`
   linearFrontier `S.union`
   goalFrontier
@@ -61,35 +100,104 @@ frontier (S uc lc goal) =
     goalFrontier = frontierPositive goal
 
 -- | Same as frontierNegative, but for opaque formulas.
-ofn :: OLFormula l a -> S.Set (DecLFormula l a)
+ofn :: OLFormula l a -> S.Set (ODecFormula l a)
 ofn (OLF f) = frontierNegative f
 
 -- | Same as frontierNegative, but for opaque left-synchronous formulas.
-olsfn :: OLSLFormula l a -> S.Set (DecLFormula l a)
+olsfn :: OLSLFormula l a -> S.Set (ODecFormula l a)
 olsfn (OLSLF f) = frontierNegative f
 
-frontierNegative :: LFormula p l a -> S.Set (DecLFormula l a)
+frontierNegative :: LFormula p l a -> S.Set (ODecFormula l a)
 frontierNegative (FAtom (RBAtom _)) = S.empty
-frontierNegative f@(FAtom (LBAtom _)) = S.singleton (LinearNegativeDLF f)
+frontierNegative f@(FAtom a@(LBAtom _)) = S.singleton (ODF (LinearNegativeAtom a))
 frontierNegative f@(FConj _ _ _) = activeNegative f
 frontierNegative (FImpl f1 f2 _) =
   frontierPositive f1 `S.union` frontierNegative f2
 
-frontierPositive :: LFormula p l a -> S.Set (DecLFormula l a)
-frontierPositive f@(FAtom (RBAtom _)) = S.singleton (LinearPositiveDLF f)
+frontierPositive :: LFormula p l a -> S.Set (ODecFormula l a)
+frontierPositive f@(FAtom a@(RBAtom _)) = S.singleton (ODF (LinearPositiveAtom a))
 frontierPositive (FAtom (LBAtom _)) = S.empty
 frontierPositive (FConj f1 f2 _) =
   frontierPositive f1 `S.union` frontierPositive f2
 frontierPositive f@(FImpl _ _ _) = activePositive f
 
-activeNegative :: LFormula p l a -> S.Set (DecLFormula l a)
-activeNegative f@(FAtom _) = S.singleton (LinearNegativeDLF f)
+activeNegative :: LFormula p l a -> S.Set (ODecFormula l a)
+activeNegative (FAtom a) = S.singleton (ODF (LinearNegativeAtom a))
 activeNegative (FConj f1 f2 _) = activeNegative f1 `S.union` activeNegative f2
 activeNegative f@(FImpl _ _ _) =
-  frontierNegative f `S.union` S.singleton (LinearNegativeDLF f)
+  frontierNegative f `S.union` S.singleton (ODF (LinearNegative f))
 
-activePositive :: LFormula p l a -> S.Set (DecLFormula l a)
-activePositive f@(FAtom _) = S.singleton (LinearPositiveDLF f)
+activePositive :: LFormula p l a -> S.Set (ODecFormula l a)
+activePositive (FAtom a) = S.singleton (ODF (LinearPositiveAtom a))
 activePositive f@(FConj _ _ _) =
-  frontierPositive f `S.union` S.singleton (LinearPositiveDLF f)
+  frontierPositive f `S.union` S.singleton (ODF (LinearPositive f))
 activePositive (FImpl f1 f2 _) = activeNegative f1 `S.union` activePositive f2
+
+--------------------------------------------------------------------------------
+-- Generation of initial rules from the frontier.
+
+{-
+
+We do not consider negative left-biased atoms (or positive right-biased atoms)
+when constructing derived rules from the formulas in the frontier.
+
+-}
+
+data ThreeWay a b c = TOne a | TTwo b | TThree c
+
+class Valid (s :: FShape) where
+instance Valid NonAtomic where
+instance Valid RBNAtom where
+instance Valid LBPAtom where
+
+data ValidDecFormula l a = forall s . (Valid s) => VDF (DecFormula s l a)
+
+decideValid :: ODecFormula l a -> Maybe (ValidDecFormula l a)
+decideValid (ODF decf) =
+  case decf of
+    UnrestrNegativeAtom (RBAtom _) -> Just . VDF $ decf
+    UnrestrNegativeAtom (LBAtom _) -> Nothing
+    UnrestrNegative _ -> Just . VDF $ decf
+    LinearNegativeAtom (RBAtom _) -> Just . VDF $ decf
+    LinearNegativeAtom (LBAtom _) -> Nothing
+    LinearNegative _ -> Just . VDF $ decf
+    LinearPositiveAtom (RBAtom _) -> Nothing
+    LinearPositiveAtom (LBAtom _) -> Just . VDF $ decf
+    LinearPositive _ -> Just . VDF $ decf
+
+genRuleFromValid :: (Eq a, Eq l) => ValidDecFormula l a -> RuleRes l a
+genRuleFromValid (VDF f) =
+  case f of
+    UnrestrNegativeAtom a -> unrestr (FAtom a)
+    UnrestrNegative f -> unrestr f
+    LinearNegativeAtom a -> linearNeg (FAtom a)
+    LinearNegative f -> linearNeg f
+    LinearPositiveAtom a -> linearPos (FAtom a)
+    LinearPositive f -> linearPos f
+  where
+    unrestr formula = do
+      (MRes gamma delta (LabelResult goal)) <- negativeFocalDispatch formula
+      return $ LS (addToUnrestrCtxt (label formula) gamma) delta goal
+    linearNeg formula = do
+      (MRes gamma delta (LabelResult goal)) <- negativeFocalDispatch formula
+      return $ LS gamma (addToLinCtxt (label formula) delta) goal
+    linearPos formula = do
+      (MRes gamma delta _) <- positiveFocalDispatch formula
+      return $ LS gamma delta (label formula)
+
+--------------------------------------------------------------------------------
+-- Main function
+
+initialSequentsAndRules
+  :: (Eq a, Eq l, Ord l, Ord a)
+  => Sequent l a -> (S.Set (LabelledSequent l a), [Rule l a])
+initialSequentsAndRules =
+  frontier >>>
+  S.toList >>>
+  map decideValid >>>
+  filterOut >>>
+  map genRuleFromValid >>>
+  map unRel >>>
+  filterOut >>>
+  fpartitionEithers >>>
+  (S.fromList *** id)
