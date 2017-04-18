cfc2c94c87c683b92ff0cfba597aef88684ebd2d
Filippo Sestini <sestini.filippo@gmail.com>
2017-04-18 10:41:16 +0200

    Prover: Rewrite all.

--- src/Prover.hs
+++ src/Prover.hs
@@ -1,48 +1,38 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 {-# OPTIONS_GHC -Wall #-}
 
 module Prover where
 
-
+import Control.Applicative
 import Control.Arrow
-import Formula
-import LabelledSequent
-import Rule
 import qualified Data.Set as S
-import qualified Data.Map as M
-import Filterable
-import Relation
-import Rel
 import Prover.Class
-import Prover.Transformer
+import Prover.Frontier
+import LabelledSequent
+import Prover.Operations
 
 -- TODO: use the real thing
-type DerivationTerm l a = Int
-
-processNewActive
-  :: (Monad m, HasProverState l a m)
-  => LabelledSequent l a -> m (RuleAppRes l a)
-processNewActive sequent = do
-  actives <- getActives
-  rules <- getRules
-  let r1 = applyAll rules sequent
-      r2 = percolate actives . resRules $ r1
-  return $ r1 `mappend` r2
+type DerivationTerm l a = LabelledSequent l a
 
 {-
 
   1. Compute frontier
   2. Compute initial set of rules ---> initial sequents
   3. Put initial sequents in inactive set
-  4. Pop inactive sequent. If no more, goto
+  4. Pop inactive sequent. If no more, exit with failure
   5. Put popped inactive in active set
   6. Apply all rules to sequent
   7. Percolate
-  8. Add resulting sequents that are not subsumed by old inactive sequents to
-     inactive set
-  9. Remove inactive sequents that are subsumed by the new sequents
+  8. Add resulting sequents that are not subsumed by old inactive sequents
+     (taken from the so-called global index) to inactive set (forward
+     subsumption)
+  9. Remove inactive sequents that are subsumed by the new sequents (backward
+     subsumption)
   10. Add rules to rule set
   11. Is any new sequent subsumed the goal, exit
   12. Add new sequents to inactive set
@@ -50,18 +40,55 @@ processNewActive sequent = do
 
 -}
 
+doSearch
+  :: forall m a l.
+     ( Monad m
+     , HasProverState l a m
+     , HasProverEnvironment l a m
+     , Ord l
+     , Ord a
+     , Eq a
+     , Eq l
+     )
+  => m (Maybe (DerivationTerm l a))
+doSearch = do
+  goal <- getGoal @l @a
+  let (initialSequents, initialRules) =
+        (S.toList *** id) . initialSequentsAndRules $ goal
+  addInactives initialSequents
+  addRules initialRules
+  (<|>) <$> (haveGoal initialSequents) <*> otterLoop
+
 otterLoop
-  :: (Monad m, HasProverState l a m, HasProverEnvironment l a m)
+  :: forall m l a.
+     ( Monad m
+     , HasProverState l a m
+     , HasProverEnvironment l a m
+     , Ord a
+     , Ord l
+     , Eq a
+     , Eq l
+     )
   => m (Maybe (DerivationTerm l a))
 otterLoop = do
-  -- TODO!!! check here if any inactive sequent subsumes the goal sequent
   inactive <- popInactive
   case inactive of
     Nothing -> return Nothing
     Just sequent -> do
       addActive sequent
-      (seqs, rules) <- processNewActive sequent
-      unsubSeqs <- filterUnsubsumed seqs
+      res <- processNewActive @m @l @a sequent
+      unsubSeqs <- filterUnsubsumed (S.toList . resSequents $ res)
+      removeSubsumedByAll unsubSeqs
       addInactives unsubSeqs
-      addRules rules
-      return (Just sequent)
+      addRules (resRules res)
+      (<|>) <$> (haveGoal unsubSeqs) <*> otterLoop
+
+processNewActive
+  :: (Monad m, HasProverState l a m, Ord l, Ord a)
+  => ActiveSequent l a -> m (RuleAppRes l a)
+processNewActive sequent = do
+  actives <- getActives
+  rules <- getRules
+  let r1 = applyAll rules (activeIsLabelled sequent)
+      r2 = percolate actives . resRules $ r1
+  return $ r1 `mappend` r2
