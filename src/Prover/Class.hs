3ba4c0fedc0715997b9fbbac4978d0af5d5c4851
Filippo Sestini <sestini.filippo@gmail.com>
2017-04-18 14:01:43 +0200

    Prover.Class: Add HasProverState and HasProverEnvironment instances for ProverT.

--- src/Prover/Class.hs
+++ src/Prover/Class.hs
@@ -25,6 +25,12 @@ import LabelledSequent
 import Rule
 import qualified Data.Set as S
 
+import Control.Monad.Trans.State.Strict hiding (get, put)
+import Control.Monad.Trans.Reader hiding (ask)
+import Control.Monad.State.Class
+import Control.Monad.Reader.Class
+import Data.Maybe (listToMaybe)
+
 -- | An active sequent is a notable kind of labelled sequent.
 newtype ActiveSequent l a = AS (LabelledSequent l a) deriving (Eq, Ord)
 type ActiveSequents l a = S.Set (ActiveSequent l a)
@@ -118,3 +124,37 @@ deriving instance (Monad m, Applicative m) => Applicative (ProverT l a m)
 deriving instance (Monad m) => Monad (ProverT l a m)
 deriving instance (Monad m) => MonadState (ProverState l a) (ProverT l a m)
 deriving instance (Monad m) => MonadReader (ProverEnvironment l a) (ProverT l a m)
+
+instance (Monad m, Ord l, Ord a) =>
+         HasProverState l a (ProverT l a m) where
+  getRules = rules <$> get
+  addRule r = do
+    (PS rls as is gi) <- get
+    put (PS (r : rls) as is gi)
+  addInactive i = do
+    (PS r as is gi) <- get
+    put (PS r as (i `S.insert` is) (i `S.insert` gi))
+  popInactive = do
+    (PS r as is gi) <- get
+    put (PS r as (S.fromList . tail . S.toList $ is) gi)
+    return . fmap AS . listToMaybe . S.toList $ is
+  getActives = do
+    (PS _ as _ _) <- get
+    return as
+  addActive a = do
+    (PS r as is gi) <- get
+    put (PS r (S.insert a as) is gi)
+  isNotSubsumed (AS s) = do
+    gi <- S.toList . globalIndex <$> get
+    return . not . or . map (\gis -> gis `subsumes` s) $ gi
+  removeSubsumedBy (AS s) = do
+    (PS r as is gi) <- get
+    let newIs = S.filter (\iseq -> not (s `subsumes` iseq)) is
+    put (PS r as newIs gi)
+
+instance (Monad m, Ord l, Ord a) =>
+         HasProverEnvironment l a (ProverT l a m) where
+  getGoal = goalSequent <$> ask
+  subsumesGoal s = do
+    gs <- convertedGoalSequent <$> ask
+    return $ s `subsumes` gs
