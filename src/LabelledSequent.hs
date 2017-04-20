56711249a8c3666854aa9da267d8d927903fc7d2
Filippo Sestini <sestini.filippo@gmail.com>
2017-04-20 17:29:15 +0200

    LabelledSequent: Make UnrestrCtxt newtype.

--- src/LabelledSequent.hs
+++ src/LabelledSequent.hs
@@ -116,8 +116,8 @@ data LabelledSequent l a =
 subsumes
   :: (Eq l, Ord l, Ord a)
   => LabelledSequent l a -> LabelledSequent l a -> Bool
-subsumes (LS uc1 lc1 l1) (LS uc2 lc2 l2) =
-  lc1 == lc2 && l1 == l2 && (uc1 <= uc2)
+subsumes (LS (UC uc1) lc1 l1) (LS (UC uc2) lc2 l2) =
+  lc1 == lc2 && l1 == l2 && (uc1 `S.isSubsetOf` uc2)
 
 {-| Ordering is defined in terms of subsumption. This works under the tacit
     assumption that subsumption is a partial ordering, and that (==) is
