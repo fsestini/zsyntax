821f4eb6721b3288316a0027eff7622db7291524
Filippo Sestini <sestini.filippo@gmail.com>
2017-04-30 23:07:38 +0200

    Add module UnrestrContext.

---
+++ src/UnrestrContext.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module UnrestrContext where
+
+import qualified Data.Set as S
+import Context
+
+newtype UnrestrCtxt a = UC (S.Set a) deriving (Eq, Ord, Monoid)
+
+instance Ord a => Context (UnrestrCtxt a) a where
+  add x (UC set) = UC (S.insert x set)
+  removeM x (UC set) = if S.member x set
+                          then return . UC $ S.delete x set
+                          else fail "element not in context"
+  asFoldable f (UC set) = f set
