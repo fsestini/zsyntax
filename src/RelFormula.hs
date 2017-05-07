7b752e7eae4f6819b594497230052aa185961b19
Filippo Sestini <sestini.filippo@gmail.com>
2017-05-08 00:52:18 +0200

    Add implication formulas

--- src/RelFormula.hs
+++ src/RelFormula.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PatternSynonyms #-}
 
 module RelFormula where
 
@@ -18,6 +20,33 @@ import Data.Foldable
    - Biological atoms
 -}
 
+data ImplKind = IAxiom | IRegular
+data BaseSpot :: (* -> *) -> ImplKind -> * -> * where
+  EmptySpot :: BaseSpot eb IAxiom a
+  FullSpot :: eb a -> BaseSpot eb IRegular a
+
+deriving instance Eq (eb a) => Eq (BaseSpot eb k a)
+deriving instance Ord (eb a) => Ord (BaseSpot eb k a)
+
+data ImplFormula :: (* -> *) -> (* -> *) -> ImplKind -> * -> * -> * where
+  ImplF
+    :: RelFormula eb cs k1 l a
+    -> BaseSpot eb ik a
+    -> cs a
+    -> RelFormula eb cs k2 l a
+    -> l
+    -> ImplFormula eb cs ik l a
+
+instance Eq (ImplFormula eb cs ik l a) where
+instance Ord (ImplFormula eb cs ik l a) where
+
+axiomIsFormula
+  :: ElemBase eb a
+  => Axiom eb cs l a -> RelFormula eb cs KImpl l a
+axiomIsFormula (ImplF a EmptySpot cs b l) = Impl a mempty cs b l
+
+type Axiom eb cs l a = ImplFormula eb cs IAxiom l a
+
 data FKind = KAtom | KConj | KImpl
 
 data RelFormula :: (* -> *) -> (* -> *) -> FKind -> * -> * -> * where
@@ -27,13 +56,11 @@ data RelFormula :: (* -> *) -> (* -> *) -> FKind -> * -> * -> * where
     -> RelFormula eb cs k2 l a
     -> l
     -> RelFormula eb cs KConj l a
-  Impl
-    :: RelFormula eb cs k1 l a
-    -> eb a
-    -> cs a
-    -> RelFormula eb cs k2 l a
-    -> l
+  Impl'
+    :: ImplFormula eb cs IRegular l a
     -> RelFormula eb cs KImpl l a
+
+pattern Impl a eb cs b l = Impl' (ImplF a (FullSpot eb) cs b l)
 
 instance Eq (RelFormula eb cs k l a) where
 instance Ord (RelFormula eb cs k l a) where
@@ -75,8 +102,15 @@ elemBaseAll = fold . map elemBase . toList
 -- Neutral formulas and sequents
 
 class IsNeutral (k :: FKind) where
+  decideNeutral
+    :: RelFormula eb cs k l a
+    -> Either (RelFormula eb cs KAtom l a) (RelFormula eb cs KImpl l a)
+
 instance IsNeutral KAtom where
+  decideNeutral = Left
+
 instance IsNeutral KImpl where
+  decideNeutral = Right
 
 data NeutralFormula eb cs l a =
   forall (k :: FKind) . IsNeutral k =>
@@ -85,11 +119,12 @@ data NeutralFormula eb cs l a =
 instance Eq (NeutralFormula eb cs l a) where
 instance Ord (NeutralFormula eb cs l a) where
 
-type UCtxt eb cs l a = UnrestrCtxt (ORelFormula eb cs l a)
+type UCtxt eb cs l a = UnrestrCtxt (Axiom eb cs l a)
 type LCtxt eb cs l a = LinearCtxt (NeutralFormula eb cs l a)
 
-data NeutralLabelledSequent eb cs l a =
-  NLS (UCtxt eb cs l a)
-      (LCtxt eb cs l a)
-      (cs a)
-      (ORelFormula eb cs l a)
+data NeutralSequent eb cs l a =
+  NS (UCtxt eb cs l a)
+     (LCtxt eb cs l a)
+     (cs a)
+     (ORelFormula eb cs l a)
+  deriving (Eq, Ord)
