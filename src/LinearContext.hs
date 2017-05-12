9327e9a10a85dbd1f1992a7b340bc7a6b6ab1bea
Filippo Sestini <sestini.filippo@gmail.com>
2017-05-12 23:21:34 +0200

    LinearContext: use Int as counter

--- src/LinearContext.hs
+++ src/LinearContext.hs
@@ -18,7 +18,6 @@ import Data.Maybe (fromMaybe)
 import Data.Semigroup
 import Data.Foldable
 import qualified Data.Map as M
-import qualified Data.List.NonEmpty as NE
 import Control.Monad.Fail
 import Prelude hiding (fail)
 import Context
@@ -28,35 +27,43 @@ import qualified TypeClasses as T (CanMap(..))
 -- Helper numeric datatype
 
 -- | Type of positive natural numbers (hence excluding zero).
-data PosNat = One | Succ PosNat deriving (Eq, Show)
+-- data PosNat = One | Succ PosNat deriving (Eq, Show)
 
-instance Semigroup PosNat where
-  One <> m = Succ m
-  Succ n <> m = Succ (n <> m)
+-- instance Semigroup PosNat where
+--   One <> m = Succ m
+--   Succ n <> m = Succ (n <> m)
 
-instance Ord PosNat where
-  compare pn1 pn2 = compare (toNum pn1 :: Int) (toNum pn2)
+-- instance Ord PosNat where
+--   compare pn1 pn2 = compare (toNum pn1 :: Int) (toNum pn2)
 
-toNum :: Num t => PosNat -> t
-toNum One = 1
-toNum (Succ n) = toNum n + 1
+-- toNum :: Num t => PosNat -> t
+-- toNum One = 1
+-- toNum (Succ n) = toNum n + 1
 
-minusOne :: PosNat -> Maybe PosNat
-minusOne One = Nothing
-minusOne (Succ n) = Just n
+-- minusOne :: PosNat -> Maybe PosNat
+-- minusOne One = Nothing
+-- minusOne (Succ n) = Just n
 
-repeatPN :: a -> PosNat -> NE.NonEmpty a
-repeatPN x One = x NE.:| []
-repeatPN x (Succ n) = (x NE.:| []) <> repeatPN x n
+-- repeatPN :: a -> PosNat -> NE.NonEmpty a
+-- repeatPN x One = x NE.:| []
+-- repeatPN x (Succ n) = (x NE.:| []) <> repeatPN x n
+
+repeatN :: a -> Int -> [a]
+repeatN x i = take i $ repeat x
+
+minusOne :: Int -> Maybe Int
+minusOne 1 = Nothing
+minusOne n | n > 1 = Just (n - 1)
+           | otherwise = error "invalid non-positive integer"
 
 --------------------------------------------------------------------------------
 
-newtype LinearCtxt a = LC (M.Map a PosNat) deriving (Eq, Ord)
+newtype LinearCtxt a = LC (M.Map a Int) deriving (Eq, Ord)
 
 instance Foldable LinearCtxt where
   foldr f z (LC lc) = foldr f z listed
     where
-      listed = M.foldMapWithKey (\x y -> NE.toList (repeatPN x y)) lc
+      listed = M.foldMapWithKey (\x y -> repeatN x y) lc
 
 subCtxtOf :: (Eq a, Ord a) => LinearCtxt a -> LinearCtxt a -> Bool
 subCtxtOf (LC m1) (LC m2) =
@@ -68,10 +75,10 @@ subCtxtOf (LC m1) (LC m2) =
 
 instance Ord a => Monoid (LinearCtxt a) where
   mempty = LC M.empty
-  (LC map1) `mappend` (LC map2) = LC $ M.unionWith (<>) map1 map2
+  (LC map1) `mappend` (LC map2) = LC $ M.unionWith (+) map1 map2
 
 instance (Ord a) => Context (LinearCtxt a) a where
-  add x (LC lc) = LC (M.insertWith (<>) x One lc)
+  add x (LC lc) = LC (M.insertWith (+) x 1 lc)
   removeM x (LC lc) = if M.member x lc
                          then return . LC $ M.update minusOne x lc
                          else fail "element not in context"
