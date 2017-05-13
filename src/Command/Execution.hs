4e06d969e1113d1747787c07fbfbbb64c8aa28b2
Filippo Sestini <sestini.filippo@gmail.com>
2017-05-13 22:01:42 +0200

    Command.Execution: Rewrite

--- src/Command/Execution.hs
+++ src/Command/Execution.hs
@@ -6,10 +6,6 @@
 
 module Command.Execution where
 
-import Text.Parsec.Char
-import Text.Parsec hiding (State, token)
-import Text.Parsec.String
-
 import qualified Data.List.NonEmpty as NE
 import Data.List.Split (splitOn)
 import Data.List
@@ -26,7 +22,6 @@ import Control.Monad.IO.Class
 import Control.Arrow ((>>>))
 import RelFormula
 import Checking
-import Text.Parsec (ParseError)
 import Data.Bifunctor
 import Control.Monad.Trans.State.Strict hiding (get, put)
 import Control.Monad.State.Class
@@ -34,23 +29,20 @@ import Control.Monad
 import Control.Monad.Identity
 import Control.Monad.Free
 import Control.Monad.Trans
-import LinearContext
-import RelFormula
 import Prover
 import Frontier
-import Context
 import DerivationTerm
 import Control.Applicative (Alternative(..))
+import Control.Monad.Trans.Except
+import Data.Foldable (toList)
 
 import Command.Structures
 import Command.Parser
 
 type MySeq a = Sequent SimpleElemBase SimpleCtrlSet a
 type MyNSeq a = NeutralSequent SimpleElemBase SimpleCtrlSet a Labels
-type MyDTSeq a =
-  DTSequent (SimpleDerTerm a) SimpleElemBase SimpleCtrlSet a Labels
-type MyUnaryRule a =
-  UnaryRule (SimpleDerTerm a) SimpleElemBase SimpleCtrlSet a Labels
+type MyDTSeq a = DTSequent (SimpleDerTerm a) SimpleElemBase SimpleCtrlSet a Labels
+type MyUnaryRule a = UnaryRule (SimpleDerTerm a) SimpleElemBase SimpleCtrlSet a Labels
 type MyGSeq a = GoalNeutralSequent SimpleElemBase SimpleCtrlSet a Labels
 type MySF a = SFormula SimpleElemBase SimpleCtrlSet a
 
@@ -71,6 +63,45 @@ type QAxiom = SAxiom SimpleElemBase SimpleCtrlSet BioAtoms
 --------------------------------------------------------------------------------
 -- Single command execution
 
+type EUI a = ExceptT String (Free UIF) a
+
+toUI :: a -> EUI a -> UI a
+toUI x = (>>= either ((>> return x) . logUI) return) . runExceptT
+
+toUI' :: (String -> UI b) -> (a -> UI b) -> EUI a -> UI b
+toUI' f g = (>>= either f g) . runExceptT
+
+toEUI :: UI a -> EUI a
+toEUI m = ExceptT (fmap Right m)
+
+liftParse :: Either String a -> EUI a
+liftParse = ExceptT . return . (first ("parse error: " ++))
+
+liftSR :: SearchRes a -> EUI a
+liftSR (OkRes x) = return x
+liftSR Saturated = ExceptT . return . Left $ "not provable: not a theorem"
+liftSR ThresholdBreak =
+  ExceptT . return . Left $ "not provable: search space too big"
+
+adjoinMsgEUI :: String -> EUI a -> EUI a
+adjoinMsgEUI str = ExceptT . fmap (adjoinMsgE str) . runExceptT
+
+adjoinMsgE :: String -> Either String a -> Either String a
+adjoinMsgE str = first (str ++)
+
+tryInsertTheorem :: ThrmName -> (QueriedSeq, SA) -> ThrmEnv -> EUI ThrmEnv
+tryInsertTheorem nm@(TN name) (q, frml) thrms =
+  maybe (throwE msg >> return thrms) return newThrms
+  where
+    newThrms = feInsert nm (q, Just frml) thrms
+    msg = "theorem named '" ++ name ++ "' already present"
+
+tryInsertAxiom :: ThrmName -> SA -> AxEnv -> EUI AxEnv
+tryInsertAxiom name@(TN nm) axiom env =
+  maybe (throwE msg) return (feInsert name axiom env)
+  where
+    msg = "axiom named '" ++ nm ++ "' already present"
+
 parseAxiom :: String -> String -> CSString
            -> Either String (SAxiom eb SimpleCtrlSet String)
 parseAxiom strFrom strTo (CSS ctrlset) = do
@@ -82,68 +113,65 @@ parseAxiom strFrom strTo (CSS ctrlset) = do
         (foldr1 sConj . fmap sAtom $ toAggr)
         ctrl
 
-addAxiom :: Env -> ThrmName -> CSString -> String -> String -> Free UIF Env
-addAxiom env (TN name) ctrlset strFrom strTo =
+addAxiom :: AxEnv -> ThrmName -> CSString -> String -> String -> UI AxEnv
+addAxiom env name@(TN nm) ctrlset strFrom strTo =
   case (parseAxiom strFrom strTo ctrlset) of
-    Left err -> (logUI $ "parseAxiom error: " ++ (show err)) >> return env
-    Right axiom -> return $ M.insert name axiom env
+    Left err -> (logUI $ "parse error: " ++ (show err)) >> return env
+    Right axiom ->
+      case feInsert name axiom env of
+        Just newEnv -> return newEnv
+        Nothing ->
+          logUI ("Axiom named '" ++ nm ++ "' already present") >> return env
 
-query :: Env -> ThrmEnv -> ThrmName -> QueriedSeq -> Free UIF (ThrmEnv)
+query :: AxEnv -> ThrmEnv -> ThrmName -> QueriedSeq -> Free UIF (ThrmEnv)
 query env thrms nm q =
-  case (queryToSequent env q) of
-    Left err -> (logUI $ "parse error: " ++ err) >> return thrms
-    Right actualSequent -> do
-      case runSearch actualSequent of
-        Saturated -> logUI "Not provable." >> return thrms
-        ThresholdBreak ->
-          logUI "Search failed: search space too big." >> return thrms
-        OkRes (DT dt _ _) ->
-          (logUI $ "Provable with " ++ (show (length impls)) ++ " transitions.") >>
-          forM_ impls (logUI . show) >>
-          return (M.insert nm q thrms)
-          where impls = transitions dt
+  flip (toUI' ((>> return thrms) . logUI)) res $ \(impls, newThrms) -> do
+    logUI ("provable with " ++ (show . length $ impls) ++ " transitions")
+    forM_ impls (logUI . show)
+    return newThrms
+  where
+    res = do
+      (DT dt _ (NS _ lc cs concl)) <-
+        liftParse (queryToSequent env thrms q) >>= liftSR . runSearch
+      let impls = transitions dt
+          formula =
+            case toList lc of
+              [] -> error "invalid non-empty linear context"
+              (x:xs) -> fromNS (x NE.:| xs) cs concl
+      newThrms <- tryInsertTheorem nm (q, formula) thrms
+      return (impls, newThrms)
 
-queryToSequent :: Env -> QueriedSeq -> Either String QSeq
-queryToSequent env (QS (AS axioms) q1 q2) = do
+processTheorems :: AxEnv -> ThrmEnv -> UI ThrmEnv
+processTheorems axioms = processThrms doer
+  where
+    doer (TN name) (q, _) thrms = fmap ((,) q) . toUI Nothing . fmap Just $
+      adjoinMsgEUI ("theorem " ++ name ++ " failed: ") (doer' axioms thrms q)
+    doer' env thrms q = do
+      actualSequent <- liftParse (queryToSequent env thrms q)
+      (DT _ _ (NS _ lc cs concl)) <- liftSR (runSearch actualSequent)
+      let formula =
+            case toList lc of
+              [] -> error "linear context should be non-empty"
+              (x:xs) -> fromNS (x NE.:| xs) cs concl
+      return formula
+
+queryToSequent :: AxEnv -> ThrmEnv -> QueriedSeq -> Either String QSeq
+queryToSequent env thrms (QS (AS axiomsString) q1 q2) = do
   lctxt <- fmap (fmap sAtom) $ parseBioAggregate1 q1
   concl <- fmap (foldr1 sConj . fmap sAtom) (parseBioAggregate1 q2)
-  axioms <- mapM (parseAxiomStr env) (splitTrim axioms)
+  axioms <- mapM (parseAxiomStr env thrms) (splitTrim axiomsString)
   return $
     SQ (fromFoldable axioms) (fromFoldable (lctxt :: NE.NonEmpty QSF)) concl
 
-changeAxiom
-  :: Env
-  -> ThrmName
-  -> CSString
-  -> String
-  -> String
-  -> ThrmEnv
-  -> Free UIF (Env, ThrmEnv)
-changeAxiom axEnv (TN axName) axCS axFrom axTo thrms =
-  case newEnvE of
-    Left err -> logUI "error parsing new axiom" >> return (axEnv, thrms)
-    Right newEnv ->
-      fmap ((,) newEnv) $ foldr (<=<) return (maps newEnv) $ mempty
-  where
-    mmm env (x, y) = toMap x y (checkThrm env y)
-    maps env = map (mmm env) (M.toList thrms)
-    newEnvE = do
-      ax <- parseAxiom axFrom axTo axCS
-      return $ M.insert axName ax axEnv
-    checkThrm env =
-      queryToSequent env >>>
-      pe >>>
-      fmap runSearch >>>
-      fmap sresToEither >>>
-      join >>> (>> return ())
-    toMap (TN name) _ (Left err) x =
-      (logUI $ "Theorem '" ++ name ++ "' is not provable anymore: " ++ err) >>
-      return x
-    toMap (TN name) s (Right ()) x =
-      (logUI $ "Theorem '" ++ name ++ "' is still provable.") >>
-      return (M.insert (TN name) s x)
+changeAxiom :: AxEnv -> ThrmName -> CSString -> String -> String -> ThrmEnv
+            -> UI (AxEnv, ThrmEnv)
+changeAxiom axEnv axName axCS axFrom axTo thrms = toUI (axEnv, thrms) $ do
+  axiom <- liftParse (parseAxiom axFrom axTo axCS)
+  newAxEnv <- tryInsertAxiom axName axiom axEnv
+  newThrms <- toEUI (processTheorems newAxEnv thrms)
+  return (newAxEnv, newThrms)
 
-loadFile :: FilePath -> StateT (Env, ThrmEnv) (Free UIF) ()
+loadFile :: FilePath -> StateT (AxEnv, ThrmEnv) (Free UIF) ()
 loadFile path = do
   contents <- lift $ uiLoadFile path
   let commandsE = mapM (parseCommand) (lines contents)
@@ -155,7 +183,7 @@ loadFile path = do
 -- Generic command execution
 
 execCommand
-  :: Command -> StateT (Env, ThrmEnv) (Free UIF) ()
+  :: Command -> StateT (AxEnv, ThrmEnv) (Free UIF) ()
 execCommand (AddAxiom name ctrlset strFrom strTo) = do
   (env, thrms) <- get
   newEnv <- lift $ addAxiom env name ctrlset strFrom strTo
@@ -184,34 +212,13 @@ instance PickMonad LabelMonad Int where
     put (i + 1)
     return i
 
-type SrchBioAtoms = Int
-type SrchLabels = Int
-
-type SrchSeq = MySeq SrchBioAtoms
-type SrchNSeq = MyNSeq SrchBioAtoms
-type SrchDTSeq = MyDTSeq SrchBioAtoms
-type SrchUnaryRule = MyUnaryRule SrchBioAtoms
-type SrchGSeq = MyGSeq SrchBioAtoms
-type SrchSF = MySF SrchBioAtoms
-type SrchAxiom = SAxiom SimpleElemBase SimpleCtrlSet SrchBioAtoms
-
-toSearchSequent :: QGSeq -> SrchGSeq
-toSearchSequent = (gnsMapAtoms hack)
-  where
-    hack "a" = 1
-    hack "b" = 2
-    hack "c" = 3
-    hack "d" = 4
-    hack "e" = 5
-    hack _ = error "no hack"
-
-runSearch :: QSeq -> SearchRes SrchDTSeq
+runSearch :: QSeq -> SearchRes QDTSeq
 runSearch actualSequent = mySearch initS initR neutral
   where
-    neutral = fst $ runState (unLM . fmap toSearchSequent . myNeutralize $ actualSequent) 0
+    neutral = fst $ runState (unLM . myNeutralize $ actualSequent) 0
     (initS, initR) = isr neutral
 
-isr :: SrchGSeq -> (S.Set (SrchDTSeq), [SrchUnaryRule])
+isr :: QGSeq -> (S.Set (QDTSeq), [QUnaryRule])
 isr = initialSequentsAndRules
 
 data SearchRes a = OkRes a | Saturated | ThresholdBreak deriving (Functor)
@@ -236,7 +243,7 @@ instance SearchMonad SearchRes where
   failSaturated = Saturated
   failThresholdBreak = ThresholdBreak
 
-mySearch :: S.Set SrchDTSeq -> [SrchUnaryRule] -> SrchGSeq -> SearchRes SrchDTSeq
+mySearch :: S.Set QDTSeq -> [QUnaryRule] -> QGSeq -> SearchRes QDTSeq
 mySearch seqs rules = runIdentity . proverSearch seqs rules
 
 myNeutralize :: QSeq -> LabelMonad QGSeq
@@ -248,24 +255,12 @@ myNeutralize = flip neutralize Nothing
 parseCtrlSet :: String -> Either String (SimpleCtrlSet BioAtoms)
 parseCtrlSet str = fmap ctrlFromFoldable . parseBioAggregate $ str
 
-parseAxiomStr :: Env -> String -> Either String QAxiom
-parseAxiomStr env str =
-  case M.lookup str env of
+parseAxiomStr :: AxEnv -> ThrmEnv -> String -> Either String QAxiom
+parseAxiomStr env thrms str =
+  case feLookup (TN str) env <|> (join . fmap snd $ feLookup (TN str) thrms) of
     Nothing -> Left $ "axiom '" ++ str ++ "' not in scope"
     Just ax -> Right ax
 
-trim = dropWhileEnd isSpace . dropWhile isSpace
-
 splitTrim :: String -> [String]
 splitTrim str = filter (not . null) $ map trim $ splitOn "," (trim str)
-
-pe :: Either String a -> Either String a
-pe = first ("parse error: " ++)
-
-applyTwo :: (a -> b) -> (a -> c) -> a -> (b, c)
-applyTwo f g x = (f x, g x)
-
-sresToEither :: SearchRes b -> Either String b
-sresToEither (OkRes x) = Right x
-sresToEither Saturated = Left ""
-sresToEither ThresholdBreak = Left "search diverged"
+  where trim = dropWhileEnd isSpace . dropWhile isSpace
