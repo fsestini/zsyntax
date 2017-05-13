6bb51642bc3857dd8902ad4a360b1fe44adfad60
Filippo Sestini <sestini.filippo@gmail.com>
2017-05-13 22:01:19 +0200

    Command.Structures: Add better implementation of environments

--- src/Command/Structures.hs
+++ src/Command/Structures.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DeriveFunctor #-}
 
 {-# OPTIONS_GHC -Wall #-}
@@ -8,6 +9,9 @@ import Control.Monad.Free
 import qualified Data.Map as M
 import SFormula
 import Checking
+import qualified Data.Dequeue as D
+import Data.Maybe (isNothing, isJust)
+import Data.Foldable (toList, foldlM)
 
 --------------------------------------------------------------------------------
 -- Command datatypes
@@ -35,8 +39,47 @@ data Command = AddAxiom ThrmName CSString String String
              | SaveToFile FilePath
              deriving (Eq, Show)
 
-type Env = M.Map String (SAxiom SimpleElemBase SimpleCtrlSet String)
-type ThrmEnv = M.Map ThrmName QueriedSeq
+type SA = SAxiom SimpleElemBase SimpleCtrlSet String
+newtype AxEnv = AE (M.Map ThrmName SA)
+newtype ThrmEnv = TE (D.BankersDequeue (ThrmName, (QueriedSeq, Maybe SA)))
+
+class FEnv env where
+  type Elems env :: *
+  feEmpty :: env
+  feInsert :: ThrmName -> Elems env -> env -> Maybe env
+  feLookup :: ThrmName -> env -> Maybe (Elems env)
+  feAsList :: env -> [(ThrmName, Elems env)]
+
+instance FEnv ThrmEnv where
+  type Elems ThrmEnv = (QueriedSeq, Maybe SA)
+  feEmpty = TE D.empty
+  feInsert nm (q, sa) (TE thrms) =
+    if isJust (lookup nm (toList thrms))
+      then Nothing
+      else Just (TE (D.pushBack thrms (nm, (q, sa))))
+  feLookup nm (TE thrms) = lookup nm (toList thrms)
+  feAsList (TE thrms) = toList thrms
+
+instance FEnv AxEnv where
+  type Elems AxEnv = SA
+  feEmpty = AE mempty
+  feInsert n x (AE env) =
+    if isJust (M.lookup n env)
+       then Nothing
+       else Just (AE (M.insert n x env))
+  feLookup x (AE env) = M.lookup x env
+  feAsList (AE m) = M.toList m
+
+processThrms
+  :: (Monad m)
+  => (ThrmName -> (QueriedSeq, Maybe SA) -> ThrmEnv -> m (QueriedSeq, Maybe SA))
+  -> ThrmEnv
+  -> m ThrmEnv
+processThrms f (TE env) = foldlM f' (TE env) (toList env)
+  where
+    f' oldenv@ (TE queue) (nm,x) = do
+      y <- f nm x oldenv
+      return (TE (D.pushBack queue (nm, y)))
 
 --------------------------------------------------------------------------------
 -- Free monad interface
