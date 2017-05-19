bd5b6c933f09b917aeb26d9c7a607d173e2d3e5f
Filippo Sestini <sestini.filippo@gmail.com>
2017-05-19 14:48:45 +0200

    Add module GUI.Command

---
+++ src/GUI/Command.hs
@@ -0,0 +1,38 @@
+module GUI.Command
+  ( AxList(..)
+  , FrmlArea(..)
+  , AxArea(..)
+  , CtrlArea(..)
+  , BioAtoms
+  , GUIElemBase
+  , GUICtrlType
+  , GUIControlType
+  , GUIAxiom
+  , GUIFormula
+  , GUIAxEnv
+  , GUIThrmEnv
+  , GUICommand
+  , Command(..)
+  , ThrmName(..)
+  , QueriedSeq(..)
+  , Elems
+  , UIF(..)
+  , UI
+  , execCommand
+  , aggregate1'
+  , neCtxt
+  , axiomList
+  ) where
+
+import Command.Structures
+import Command.Execution
+import CLI.Command
+
+type GUICommand = CLICommand
+type GUIElemBase = UIElemBase
+type GUICtrlType = UICtrlType
+type GUIAxiom = UIAxiom
+type GUIFormula = UIFormula
+type GUIControlType = ControlType
+type GUIAxEnv = CLIAxEnv
+type GUIThrmEnv = CLIThrmEnv
