86a85812e4b854c5df1c2cbcebab0e05b555c400
Filippo Sestini <sestini.filippo@gmail.com>
2017-06-08 18:54:33 +0200

    LinearContext.LinearCtxt: Update types of subCtxtOf.

--- src/LinearContext/LinearCtxt.hs
+++ src/LinearContext/LinearCtxt.hs
@@ -44,15 +44,12 @@ instance Foldable LinearCtxt where
     where
       listed = M.foldMapWithKey (\x y -> repeatN x y) lc
 
-lcSubCtxtOf :: (Ord a) => LinearCtxt a -> LinearCtxt a -> [a]
-lcSubCtxtOf (LC m1) (LC m2) =
-  if m1 == m2
-    then []
-    else concatMap duane (M.toList m1) -- and $ map duane (M.toList m1)
+lcSubCtxtOf :: (Ord a) => LinearCtxt a -> LinearCtxt a -> SubCtxt a
+lcSubCtxtOf lc1 lc2 =
+  SC (toList . eiOnFirst $ eqRes) (toList . eiOnBoth $ eqRes)
   where
-    duane (key, pi1) =
-      flip (maybe (repeatN key pi1)) (M.lookup key m2) $ \pi2 ->
-        take (toInt pi2 - toInt pi1) (repeat key)
+    eqRes = lcEqCtxt lc1 lc2
+
 instance Ord a => Eq' (LinearCtxt a) where
   eq' lc1 lc2 = fmap fromFoldable $ lcEqCtxt lc1 lc2
 
@@ -152,7 +149,9 @@ instance Ord elem => Context (NonEmptyLinearCtxt elem) where
 instance Ord elem => Semigroup (NonEmptyLinearCtxt elem) where
   m1 <> m2 = merge m1 m2
 
-neSubCtxtOf :: Ord a => NonEmptyLinearCtxt a -> NonEmptyLinearCtxt a -> [a]
+neSubCtxtOf
+  :: Ord a
+  => NonEmptyLinearCtxt a -> NonEmptyLinearCtxt a -> SubCtxt a
 neSubCtxtOf nelc1 nelc2 = subCtxtOf (toLC nelc1) (toLC nelc2)
 
 toNEList :: NonEmptyLinearCtxt elem -> NE.NonEmpty elem
