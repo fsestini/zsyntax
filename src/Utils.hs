756325759cc5d33575286d74740c5d355f482b43
Filippo Sestini <sestini.filippo@gmail.com>
2017-06-08 18:26:15 +0200

    Utils: Add operator (.:).

--- src/Utils.hs
+++ src/Utils.hs
@@ -9,3 +9,7 @@ trim = dropWhileEnd isSpace . dropWhile isSpace
 maySingleton :: [a] -> Maybe a
 maySingleton [x] = Just x
 maySingleton _ = Nothing
+
+infix 8 .:
+(.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+(f .: g) x y = f (g x y)
