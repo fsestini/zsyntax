eadc7ca1e9313c3df3d839133d1fa9ca957c31ed
Filippo Sestini <sestini.filippo@gmail.com>
2017-06-08 18:47:35 +0200

    Utils: Add operator (&&&).

--- src/Utils.hs
+++ src/Utils.hs
@@ -13,3 +13,7 @@ maySingleton _ = Nothing
 infix 8 .:
 (.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
 (f .: g) x y = f (g x y)
+
+infix 8 &&&
+(&&&) :: (a -> b) -> (a -> c) -> a -> (b,c)
+(f &&& g) x = (f x, g x)
