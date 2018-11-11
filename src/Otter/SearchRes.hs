3b126cac392c72ce1cc202bf1d7a0142b4fa33e0
Filippo Sestini <sestini.filippo@gmail.com>
2018-11-11 22:21:04 +0000

    s/SpaceExhausted/FailureReason/

--- src/Otter/SearchRes.hs
+++ src/Otter/SearchRes.hs
@@ -4,7 +4,7 @@ module Otter.SearchRes
   ( SearchRes
   , Res(..)
   , Extraction(..)
-  , SpaceExhausted(..)
+  , FailureReason(..)
   , extractResults
   , delay
   , cons
@@ -45,19 +45,19 @@ cons x = (Res x :)
 -- | Type of reasons why no result can be extracted from a search result stream.
 -- 
 -- Either the search space has been exhaustively searched and no result was
--- found, or the search was terminated preemptively according to an upper bound
--- imposed on the maximum depth of the search space.
-data SpaceExhausted = Exhausted | NotExhausted
+-- found (the query is not a theorem), or the search was terminated preemptively
+-- according to an upper bound imposed on the maximum depth of the search space.
+data FailureReason = NotATheorem | SpaceTooBig
 
 -- | Type of the result of 'extractResults', which extracts all positive search
 -- results from a search result stream.
 -- An element of `Extraction a` is either a non-empty list of positive results,
 -- or an element of 'SpaceExhausted' giving a reason why no result was found.
-data Extraction a = AllResults (NonEmpty a) | NoResults SpaceExhausted
+data Extraction a = AllResults (NonEmpty a) | NoResults FailureReason
 
-resListUntil :: Int -> SearchRes a -> ([a], SpaceExhausted)
-resListUntil _ [] = ([], Exhausted)
-resListUntil 0 (_ : _)  = ([], NotExhausted)
+resListUntil :: Int -> SearchRes a -> ([a], FailureReason)
+resListUntil _ [] = ([], NotATheorem)
+resListUntil 0 (_ : _)  = ([], SpaceTooBig)
 resListUntil i (Res x : xs) = let (ys, b) = resListUntil i xs in (x : ys, b)
 resListUntil i (Delay : xs) = resListUntil (i - 1) xs
 
